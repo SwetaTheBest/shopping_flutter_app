e2d57ffbb6fa9eec942504bab3db20f894e0999c
Sweta Jain <swetajain04@gmail.com>
2021-01-25 22:53:40 +0530

    Google maps: key update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyA9A_3xeZ5qoyK_0JaB0wLxeCyyYDjYxw0")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
